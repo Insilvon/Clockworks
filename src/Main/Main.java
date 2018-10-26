0fb1d80fb9b0a504ab29ec9c539f816538321df3
Insilvon <colincrowe14@gmail.com>
2018-10-26 00:23:02 -0400

    __init__

---
+++ src/Main/Main.java
@@ -0,0 +1,9 @@
+package Main;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        COSC_241.Project2EC.run();
+    }
+}
